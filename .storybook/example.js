674d1fcd2106c65dcfd097e596639d6fde43d0c8
akameco <akameco.t@gmail.com>
2017-05-10 22:37:59 +0900

    Fix test config

--- .storybook/example.js
+++ .storybook/example.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@kadira/storybook'
 import { text, number } from '@kadira/storybook-addon-knobs'
-import Components from '../src'
+import * as Components from '../src'
 
 const removePrefix = (name: string) => name.replace('__styled-spinkit__', '')
 
