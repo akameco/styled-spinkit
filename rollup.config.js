8194632281cca2272bad1548f2048dd9e506f1e0
akameco <akameco.t@gmail.com>
2017-05-12 02:12:29 +0900

    Add rollup build

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import flow from 'rollup-plugin-flow'
+
+const targets = [{ dest: 'dist/styled-spinkit.es.js', format: 'es' }]
+
+const plugins = [
+  flow(),
+  nodeResolve({ jsnext: true }),
+  commonjs(),
+  babel({
+    babelrc: false,
+    presets: [['env', { modules: false }], 'react'],
+    plugins: [
+      'flow-react-proptypes',
+      'transform-flow-strip-types',
+      'external-helpers',
+      'transform-class-properties',
+    ],
+  }),
+]
+
+export default {
+  entry: 'src/index.js',
+  external: ['react'],
+  exports: 'named',
+  targets,
+  plugins,
+  globals: { react: 'React' },
+}
