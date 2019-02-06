6d27e65fd9acaaf8ae15e73c5e14554015327aa2
akameco <akameco.t@gmail.com>
2019-02-07 02:45:55 +0900

    chore: remove storybook

--- src/ThreeBounce/index.tsx
+++ src/ThreeBounce/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { SpinkitProps, DEFAULT_SIZE, DEFAULT_COLOR } from '../types'
 import { Child, StyledThreeBounce } from './styles'
 
-export const ThreeBounce: React.FC<SpinkitProps> = ({
+const ThreeBounce: React.FC<SpinkitProps> = ({
   size = DEFAULT_SIZE,
   color = DEFAULT_COLOR,
 }) => {
